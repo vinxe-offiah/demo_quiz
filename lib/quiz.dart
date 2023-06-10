ca6bbce26c1ac2273e4225544880dfd36ef80606
chineme-offiah <voffiah@bluetag.tech>
2023-06-10 15:18:02 +0100

    initial commit

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,13 +4,24 @@ class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
 }
 
-
 class _QuizState extends State<Quiz> {
-
   @override
   Widget build(context) {
-    return MaterialApp(home: Scaffold(body: Container(decoration: const BoxDecoration(gradient: LinearGradient(colors: [Colors.deepPurple, Colors.deepPurpleAccent],),),),),);
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.deepPurpleAccent],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
