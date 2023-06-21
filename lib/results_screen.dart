0e0b62f581b65e09341b8de2666e44beab2ee6b8
VINCENT OFFIAH <offiahvin@gmail.com>
2023-06-21 23:09:00 +0100

    Adding Result Screen

---
+++ lib/results_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.all(30),
+        child: const Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
